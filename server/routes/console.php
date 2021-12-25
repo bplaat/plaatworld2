f8f40e5f86048b2356193876d25d9029cc8e1b7b
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2021-12-25 14:28:25 +0100

    Add laravel cache production files alias command

--- server/routes/console.php
+++ server/routes/console.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Artisan;
 
 // Helpers
+Artisan::command('prod', function () {
+    Artisan::call('optimize');
+    Artisan::call('view:cache');
+})->purpose('Cache stuff for production');
+
 Artisan::command('websockets:dev', function () {
     system('nodemon -w app -e php --exec php artisan websockets:serve');
 })->purpose('Run the WebSocket server with dev live refresh');
